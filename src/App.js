3612c39677d197e7114f28ad6939fde6295931dd
preethichals <108888412+preethichals@users.noreply.github.com>
2023-06-19 23:00:40 +0530

    Project-Disney-02

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import Home from './pages/Home';
+import { Routes,Route } from 'react-router';
+import Login from './pages/Login';
+import Detail from './pages/MoviesDetail';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+   <Routes>
+   <Route path="/" element={<Home/>} />
+   <Route path="/login" element={<Login/>}/>
+   <Route path="/movie-detail" element={<Detail/>}/>
+   </Routes>
+  
+   
+
   );
 }
 
